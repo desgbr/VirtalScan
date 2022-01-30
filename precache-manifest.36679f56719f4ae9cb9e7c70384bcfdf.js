76d1278091f94e36308201599570af46513a3e21
desgbr <des.gbr@gmail.com>
2022-01-30 22:55:42 +0200

    Updates

---
+++ precache-manifest.36679f56719f4ae9cb9e7c70384bcfdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5e9bb57414ac9bb7d4a622b2362b5f",
+    "url": "/VirtalScan/index.html"
+  },
+  {
+    "revision": "d468fcc420281501ddc6",
+    "url": "/VirtalScan/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "dd4f7d35c3810af8bafd",
+    "url": "/VirtalScan/static/css/main.2cd8cbfa.chunk.css"
+  },
+  {
+    "revision": "d468fcc420281501ddc6",
+    "url": "/VirtalScan/static/js/2.9e30c04c.chunk.js"
+  },
+  {
+    "revision": "dd4f7d35c3810af8bafd",
+    "url": "/VirtalScan/static/js/main.c7588bd3.chunk.js"
+  },
+  {
+    "revision": "4647d306b07d653a919d",
+    "url": "/VirtalScan/static/js/runtime~main.5076f1ca.js"
+  }
+]);
